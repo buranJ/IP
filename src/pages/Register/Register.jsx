ddcbe24c795ac386d8a5985691b0a6d695cbb37d
Bekich141 <123182789+Bekich141@users.noreply.github.com>
2025-03-01 21:11:09 +0600

    сделано 2-секция в регистре

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -7,14 +7,14 @@ function Register({ children }) {
       <section className="register bg-black">
         <div className="container">
           <div className="register__content">
-            <h2 className="register__title font-second font-normal text-[24px] leading-[135%] text-white">
+            <h2 className="register__title ml-[40px] font-second font-normal text-[24px] leading-[135%] text-white md:text-[38px]">
               Когда необходимо
             </h2>
-            <div className="register__box mt-[50px] flex flex-wrap gap-[40px] justify-start sm:justify-center md:justify-start lg:justify-center xl:justify-start">
+            <div className="register__box mt-[50px] flex flex-col gap-[40px] md:ml-[10px] md:flex-row md:justify-center">
               {registerData.map((item) => (
                 <div
                   key={item.id}
-                  className="register__frame gap-[20px] w-full sm:w-[45%] md:w-[30%] lg:w-[22%] xl:w-[18%] bg-[#1B1B1B] h-[181px] p-6 rounded-lg border border-red-500/50 text-white"
+                  className="register__frame gap-[20px] w-[370px] bg-[#1B1B1B] h-[170px] p-6 rounded-lg border border-red-500/50 text-white"
                 >
                   <h4 className="font-main font-medium text-[18px] leading-[160%] text-white">
                     {item.title}
@@ -31,5 +31,4 @@ function Register({ children }) {
     </>
   );
 }
-
 export default Register;
