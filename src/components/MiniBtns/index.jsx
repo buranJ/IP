4b4411420f8687c1395720c24ca1fa7d93877cd5
esenbaev <esenbaev132@gmail.com>
2025-03-07 23:35:39 +0600

    fix minibtn

--- src/components/MiniBtns/index.jsx
+++ src/components/MiniBtns/index.jsx
@@ -3,19 +3,19 @@ import Btn2 from '../../assets/img/Bisness/rightbtn.svg';
 
 
 
-function MiniBtns({ swiperInstance, gap={} }) {
+function MiniBtns({ swiperInstance, gap={}, color }) {
     return (
         <div className={`minibuttons ${gap} mt-[25px] flex gap-[10px] justify-center`}>
             <button className="btn-mini-left flex items-center gap-[10px] justify-center"
                 onClick={() => swiperInstance?.slidePrev()}
             >
-                <img  
+                <img  style={{backgroundColor: color}}
                 src={Btn1} alt="Previous" />
             </button>
             <button className="btn-mini-right flex items-center gap-[10px] justify-center"
                 onClick={() => swiperInstance?.slideNext()}
             >
-                <img 
+                <img style={{backgroundColor: color}}
                 src={Btn2} alt="Next" />
             </button>
         </div>
